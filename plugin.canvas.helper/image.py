c51f24cf1d969273f2bb5a48cb9dbbe6ebf6b3ac
Gabriele Seppi <gabriele.seppi@outlook.com>
2025-07-21 21:10:40 +0200

    New, more flexible approach
    All items/lists are loaded via plugin with JSON-RPC queries and creation of modified art + additional info to use in skin

---
+++ plugin.canvas.helper/image.py
@@ -0,0 +1,108 @@
+import io, os
+import xbmc, xbmcvfs
+import urllib.request as urllib
+from PIL import Image, ImageFilter
+
+# Searches the required image, returns its full path and potential edited cache path.
+def get_image(imgPath):
+    try:
+        # Some paths require unquoting to get a valid cached thumb hash.
+        if imgPath.startswith("image://"):
+            imgPath = urllib.unquote(imgPath.replace('image://', ''))
+            if imgPath.endswith('/'):
+                imgPath = imgPath[:-1]
+
+        # Get thumbnail path in cache.
+        cache = xbmcvfs.translatePath('special://profile/Thumbnails')
+        thumb = xbmc.getCacheThumbName(imgPath)
+        thumb = thumb[:-4]
+        full_path = os.path.join(cache, thumb[0], thumb + '.jpg')
+        if (xbmcvfs.exists(full_path)):
+            thumb = thumb + '.jpg'
+        else:
+            full_path = os.path.join(cache, thumb[0], thumb + '.png')
+            if (xbmcvfs.exists(full_path)):
+                thumb = thumb + '.png'
+            else:
+                full_path = os.path.join(cache, thumb[0], thumb + '.jpeg')
+                if (xbmcvfs.exists(full_path)):
+                    thumb = thumb + '.jpeg'
+                else:
+                    return
+
+        # Compute output path from input path.
+        out = xbmcvfs.translatePath('special://temp/temp/' + thumb)
+
+        return (full_path, out)
+    except:
+        return
+
+# Takes an art path, downsizes it, blurs it, saves in into temp and returns the new path.
+# It avoids re-sizing, re-blurring if already in cache.
+def get_blurred(imgPath):
+    try:
+        # Load paths.
+        full_path, out = get_image(imgPath)
+
+        # Check if output already present. If so, use it.
+        if xbmcvfs.exists(out):
+            return out
+
+        # Resize and blur.
+        with xbmcvfs.File(xbmcvfs.translatePath(full_path), 'rb') as f:
+            image_bytes = f.readBytes()
+        img = Image.open(io.BytesIO(image_bytes))
+        img = img.resize((int(480), int(270)), Image.LANCZOS)
+        img = img.filter(ImageFilter.GaussianBlur(radius=float(80)))
+
+        # Save output.
+        img.save(out, 'JPEG')
+
+        # Return output path.
+        return out
+    except:
+        return ''
+
+# Takes an clearlogo path, crops it to the actual content, saves in into temp and returns the new path.
+# It creates 2 version, the original size, cropped, and a smaller one, cropped as well.
+# It avoids re-cropping if already in cache.
+def get_cropped_clearlogo(imgPath):
+    try:
+        # Load paths.
+        full_path, out = get_image(imgPath)
+        out_small = out.replace('.png', '-small.png')
+
+        # Check if output already present. If so, use it.
+        done = True
+        if not xbmcvfs.exists(out):
+            done = False
+        if not xbmcvfs.exists(out_small):
+            done = False
+        if done:
+            return (out, out_small)
+
+        # Crop.
+        with xbmcvfs.File(xbmcvfs.translatePath(full_path), 'rb') as f:
+            image_bytes = f.readBytes()
+        img = Image.open(io.BytesIO(image_bytes))
+        try:
+            # Errors with single channel L conversion to RGBa so catch exceptions
+            img_rgba = img.convert('RGBa')
+            img = img.crop(img_rgba.getbbox())
+        except Exception:
+            # If we get a conversion error just try getting bounding box with current channel
+            # We'll probably be okay with single channel texture since Kodi now handles these better
+            img = img.crop(img.getbbox())
+
+        # Resize for small version.
+        width, height = img.size
+        img_small = img.resize((180, int(180*height/width)), Image.LANCZOS)
+
+        # Save output.
+        img.save(out, 'PNG')
+        img_small.save(out_small, 'PNG')
+
+        # Return output paths.
+        return (out, out_small)
+    except:
+        return ('', '')
--- plugin.canvas.helper/plugin.py
+++ plugin.canvas.helper/plugin.py
@@ -1,119 +1,257 @@
-#import xbmcgui, xbmcplugin, json, urllib.request
-#from urllib.parse import parse_qs
 import sys
+import math
 import json
 import xbmc
 import xbmcgui
 import xbmcplugin
-from urllib.parse import parse_qs
+from image import get_blurred, get_cropped_clearlogo
 
 handle = int(sys.argv[1])
 
+# Default property lists.
+movie_properties_query = ['title', 'year', 'resume', 'art', 'plot', 'studio', 'streamdetails', 'mpaa', 'genre']
+episode_properties_query = ['art', 'showtitle', 'title', 'season', 'episode', 'firstaired', 'studio', 'playcount', 'resume', 'streamdetails', 'plot', 'dateadded']
+
 # Calls a JSON-RPC method agains Kodi.
 def call_rpc(method, params=None):
-        payload = {
-            'jsonrpc': '2.0',
-            'method': method,
-            'id': 1,
-            'params': params or {}
-        }
-        r = xbmc.executeJSONRPC(json.dumps(payload))
-        return json.loads(r).get('result', {})
+    payload = {
+        'jsonrpc': '2.0',
+        'method': method,
+        'id': 1,
+        'params': params or {}
+    }
+    r = xbmc.executeJSONRPC(json.dumps(payload))
+    return json.loads(r).get('result', {})
+
+# Build a listitem for movies.
+def get_movie_listitem(movie):
+    # Create movie-type listitem.
+    li = xbmcgui.ListItem(label=movie['title'])
+    li.setProperty('ItemType', 'movie')
+
+    # Set internal properties.
+    li.setArt(movie['art'])
+    videoinfo = li.getVideoInfoTag()
+    videoinfo.setTitle(movie['title'])
+    videoinfo.setYear(movie['year'])
+    videoinfo.setStudios(movie['studio'])
+    videoinfo.setPlot(movie['plot'])
+    videoinfo.setGenres(movie['genre'])
+    videoinfo.setDuration(movie['streamdetails']['video'][0]['duration'])
+    videoinfo.setResumePoint(movie['resume']['position'], movie['resume']['total'])
+
+    # Get custom art.
+    blur = get_blurred(movie['art'].get('fanart', ''))
+    clearlogo, clearlogo_small = get_cropped_clearlogo(movie['art'].get('clearlogo', ''))
+
+    # Compute duration visual string.
+    duration = movie['streamdetails']['video'][0]['duration']
+    if duration >= 3600:
+        hours = math.floor(duration / 3600)
+        minutes = math.floor((duration - (hours * 3600)) / 60)
+        duration = f"{hours}h{minutes}m"
+    else:
+        duration = f"{math.floor(duration / 60)}m"
+
+    # Remove "Rated" from rating.
+    rating = movie['mpaa']
+    if rating is not None:
+        rating = rating.replace('Rated ', '')
+    else:
+        rating = ''
+
+    # Set custom properties.
+    videoinfo.setMpaa(rating)
+    li.setProperty('DurationString', duration)
+    li.setProperty('BlurArt', blur)
+    li.setProperty('Clearlogo.Big', clearlogo)
+    li.setProperty('Clearlogo.Small', clearlogo_small)
+        
+    return li
+
+# Build a listitem for TV shows.
+def get_tvshow_listitem():
+    #todo
+    a = 0
+
+# Build a listitem for seasons.
+def get_season_listitem():
+    #todo
+    a = 0
+
+# Build a listitem for episodes.
+def get_episode_listitem(episode):
+    # Create movie-type listitem.
+    li = xbmcgui.ListItem(label=episode['title'])
+    li.setProperty('ItemType', 'episode')
+
+    # Set internal properties.
+    li.setArt(episode['art'])
+    videoinfo = li.getVideoInfoTag()
+    videoinfo.setTitle(episode['title'])
+    videoinfo.setSeason(episode['season'])
+    videoinfo.setEpisode(episode['episode'])
+    videoinfo.setPremiered(episode['firstaired'])
+    videoinfo.setStudios(episode['studio'])
+    videoinfo.setPlot(episode['plot'])
+    videoinfo.setTvShowTitle(episode['showtitle'])
+    videoinfo.setDuration(episode['streamdetails']['video'][0]['duration'])
+    videoinfo.setResumePoint(episode['resume']['position'], episode['resume']['total'])
+
+    # Get custom art.
+    tvshow_blur = get_blurred(episode['art'].get('tvshow.fanart', ''))
+    season_blur = get_blurred(episode['art'].get('season.fanart', ''))
+    clearlogo, clearlogo_small = get_cropped_clearlogo(episode['art'].get('tvshow.clearlogo', ''))
+
+    # Compute duration visual string.
+    duration = episode['streamdetails']['video'][0]['duration']
+    if duration >= 3600:
+        hours = math.floor(duration / 3600)
+        minutes = math.floor((duration - (hours * 3600)) / 60)
+        duration = f"{hours}h{minutes}m"
+    else:
+        duration = f"{math.floor(duration / 60)}m"
+
+    # Remove "Rated" from rating.
+    rating = episode.get('tvshow', {'mpaa': None})['mpaa']
+    if rating is not None:
+        rating = rating.replace('Rated ', '')
+    else:
+        rating = ''
+
+    # Set custom properties.
+    li.setArt({'poster': episode['art'].get('tvshow.poster', '')})
+    videoinfo.setMpaa(rating)
+    li.setProperty('DurationString', duration)
+    li.setProperty('BlurArt.TvShow', tvshow_blur)
+    li.setProperty('BlurArt.Season', season_blur)
+    li.setProperty('Clearlogo.Big', clearlogo)
+    li.setProperty('Clearlogo.Small', clearlogo_small)
+        
+    return li
+
+# Build a listitem for music albums.
+def get_album_listitem():
+    #todo
+    a = 0
+
+# Build a listitem for songs.
+def get_song_listitem():
+    #todo
+    a = 0
+
+# Build a listitem for pictures.
+def get_picture_listitem():
+    #todo
+    a = 0
 
 # Create a list of continue watching items: movies in progress, episodes in progress,
 # TV shows in progress. It collapses TV shows and episodes into one single item if
 # from the same TV show.
 def list_continue_watching():
-    movies = call_rpc('VideoLibrary.GetMovies', {'properties': ['title', 'year', 'resume', 'art', 'plot', 'studio', 'streamdetails', 'mpaa', 'genre']})
-    shows = call_rpc('VideoLibrary.GetInprogressTVShows', {'properties': ['title', 'art', 'mpaa']})
-
-    filtered_movies = [
+    # All movies, then filter them for those in progress.
+    movies = call_rpc('VideoLibrary.GetMovies', {'properties': movie_properties_query})
+    movies = [
         m for m in movies.get('movies', [])
         if 0 < m['resume']['position'] < m['resume']['total']
     ]
 
-    next_up = []
-    for show in shows.get('tvshows', []):
-        eps = call_rpc('VideoLibrary.GetEpisodes', {
-            'tvshowid': show['tvshowid'],
+    # In progress TV shows.
+    tvshows = call_rpc('VideoLibrary.GetInprogressTVShows', {'properties': ['mpaa']})
+    # For each show, find the next episode.
+    next_episodes = []
+    for tvshow in tvshows.get('tvshows', []):
+        next_episode = call_rpc('VideoLibrary.GetEpisodes', {
+            # Filter by show ID.
+            'tvshowid': tvshow['tvshowid'],
+            # Then find those with a playcount of 0, excluding specials.
             'filter': {
                 'and': [
                     {'field': 'playcount', 'operator': 'is', 'value': "0"},
                     {'field': 'season', 'operator': 'greaterthan', 'value': "0"}
                 ]
             },
+            # Sort by episode, ascending.
             'sort': {'order': 'ascending', 'method': 'episode'},
-            'properties': [
-                'title',
-                'season',
-                'episode',
-                'firstaired',
-                'studio',
-                'playcount',
-                'resume',
-                'streamdetails',
-                'plot'
-            ],
-            'limits': {'start': 0, 'end': 1}
+            # Pick the first.
+            'limits': {'start': 0, 'end': 1},
+            'properties': episode_properties_query
         }).get('episodes', [])
 
-        candidate = None
-        for e in eps:
-            if 0 < e['resume']['position'] < e['resume']['total']:
-                candidate = e
-                break
-            if e['playcount'] == 0:
-                candidate = e
-                break
+        if next_episode is not None and len(next_episode) > 0:
+            next_episode = next_episode[0]
+            next_episode['tvshow'] = tvshow
+            next_episodes.append(next_episode)
 
-        if candidate:
-            candidate['show_title'] = show['title']
-            candidate['art'] = show['art']
-            candidate['mpaa'] = show['mpaa']
-            next_up.append(candidate)
-
-    for movie in filtered_movies:
-        li = xbmcgui.ListItem(label=movie["label"])
-        li.setArt(movie["art"])
-        videoinfo = li.getVideoInfoTag()
-        videoinfo.setTitle(movie["title"])
-        videoinfo.setYear(movie["year"])
-        videoinfo.setStudios(movie["studio"])
-        videoinfo.setPlot(movie["plot"])
-        videoinfo.setMpaa(movie["mpaa"])
-        videoinfo.setGenres(movie["genre"])
-        videoinfo.setDuration(movie["streamdetails"]["video"][0]["duration"])
-        
+    #todo: sort movies, episodes by last viewed, then add
+    
+    for movie in movies:
+        li = get_movie_listitem(movie)
         xbmcplugin.addDirectoryItem(
             handle=handle,
-            url=movie["title"],
+            url=movie['title'],#todo
             listitem=li,
             isFolder=False
         )
 
-    for episode in next_up:
-        li = xbmcgui.ListItem(label=episode["label"])
-        li.setArt(episode["art"])
-        videoinfo = li.getVideoInfoTag()
-        videoinfo.setTitle(episode["show_title"])
-        videoinfo.setSeason(episode["season"])
-        videoinfo.setEpisode(episode["episode"])
-        videoinfo.setPremiered(episode["firstaired"])
-        videoinfo.setStudios(episode["studio"])
-        videoinfo.setPlot(episode["plot"])
-        videoinfo.setMpaa(episode["mpaa"])
-        videoinfo.setDuration(episode["streamdetails"]["video"][0]["duration"])
-        videoinfo.setResumePoint(episode["resume"]["position"], episode["resume"]["total"])
-        li.setProperty("EpisodeTitle", episode["title"])
-        
+    for episode in next_episodes:
+        li = get_episode_listitem(episode)
         xbmcplugin.addDirectoryItem(
             handle=handle,
-            url=episode["title"],
+            url=episode['title'],#todo
             listitem=li,
             isFolder=False
         )
 
-    #todo: sort according to last played
+    xbmcplugin.endOfDirectory(handle)
+
+# Create a list of recently added TV show episodes.
+def recently_added_tvshow_episode():
+    # Load all episodes.
+    all_episodes = call_rpc('VideoLibrary.GetEpisodes', {
+        # Sort by date added, descending.
+        'sort': {'order': 'descending', 'method': 'dateadded'},
+        # Only get TV show title.
+        'properties': ['showtitle']
+    }).get('episodes', [])
+
+    # Get first 20 episodes that are not Yoga with Adriene.
+    episodes = []
+    count = 0
+    for e in all_episodes:
+        # Stop at 20.
+        if count == 20:
+            break
+        
+        # If not Yoga with Adriene, add to list.
+        if e['showtitle'].lower() != 'yoga with adriene':
+            episodes.append(e)
+            count += 1
+
+    if len(episodes) > 0:
+        # Load all TV shows.
+        all_shows = call_rpc('VideoLibrary.GetTVShows', {
+            # Only get TV show title and rating for later use.
+            'properties': ['title', 'mpaa']
+        }).get('tvshows', [])
+        # Create map of shows.
+        shows = {}
+        for s in all_shows:
+            shows[s['title']] = s
+
+        # For each episode ID found, get details and add to list.
+        for e in episodes:
+            episode = call_rpc('VideoLibrary.GetEpisodeDetails', {'episodeid': e['episodeid'], 'properties': episode_properties_query}).get('episodedetails', None)
+            if episode is not None:
+                # Add show info, then create listitem.
+                episode['tvshow'] = shows[e['showtitle']]
+                li = get_episode_listitem(episode)
+                xbmcplugin.addDirectoryItem(
+                    handle=handle,
+                    url=episode['title'],#todo
+                    listitem=li,
+                    isFolder=False
+                )
 
     xbmcplugin.endOfDirectory(handle)
 
@@ -129,5 +267,7 @@ if __name__ == '__main__':
 
     if method == 'continue_watching':
         list_continue_watching()
+    elif method == 'recently_added_tvshow_episode':
+        recently_added_tvshow_episode()
     elif method == 'yoga_with_adriene':
         list_yoga_with_adriene()
